a61cc54a23daa444fed8a9d83098066e2497f37f
codazoa <codazoa@protonmail.com>
2023-03-28 14:36:11 -0700

    Basic structure and example file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # OS-Scheduler
+
+### To run:
+`$ ./scheduler -alg [FCFS,SJF,PR,RR] [-quantum [integer(ms)] -input [filename]`
+
+Available Scheduling Algorithms:
+- First Come First Serve
+- Shortest Job First
+- Priority Scheduling
+- Round Robin
+
+Quantum is only used for RR (Round Robin) scheduling algorithm
--- file_parser.c
+++ file_parser.c
@@ -1,1 +1,13 @@
 #include "file_parser.h"
+
+// Sample File Format
+// proc PRIORITY NUM_OF_BURSTS CPU_BURST IO_BURST CPU_BURST . . .
+// sleep TIME_TO_SLEEP_ms
+// stop
+// 
+// proc: a process to simulate, always ends on a CPU_BURST
+// PRIORITY: 1-10, 1-lowest 10-highest 5-average
+// NUM_OF_BURSTS: how many CPU and IO bursts will follow
+// CPU_BURST: CPU burst time in ms
+// IO_BURST: IO burst time in ms
+// TIME_TO_SLEEP: how many ms for the parser thread to sleep
