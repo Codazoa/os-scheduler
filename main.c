95561e11bf5029e15786762a95c38de29c746cbe
codazoa <codazoa@protonmail.com>
2023-03-28 20:32:32 -0700

    Styling fix

--- main.c
+++ main.c
@@ -25,19 +25,19 @@ int main(int argc, char const *argv[]) {
         // algorithm option
         if( strcmp(argv[i], "-alg") == 0 ){
             // check next argument for algorithm to use
-            if( strcmp(argv[i+1], "FCFS") == 0 ){
+            if (strcmp(argv[i+1], "FCFS") == 0) {
                 if(DEBUG) { printf("Activating FIRST COME FIRST SERVE\n"); }
                 algo = 1;
             }
-            else if( strcmp(argv[i+1], "SJF") == 0 ){
+            else if (strcmp(argv[i+1], "SJF") == 0) {
                 if(DEBUG) { printf("Activating SHORTEST JOB FIRST\n"); }
                 algo = 2;
             }
-            else if( strcmp(argv[i+1], "PR") == 0 ){
+            else if (strcmp(argv[i+1], "PR") == 0) {
                 if(DEBUG) { printf("Activating PRIORITY SCHEDULING\n"); }
                 algo = 3;
             }
-            else if( strcmp(argv[i+1], "RR") == 0 ){
+            else if (strcmp(argv[i+1], "RR") == 0) {
                 if(DEBUG) { printf("Activating ROUND ROBIN\n"); }
                 algo = 4;
             }
@@ -49,21 +49,21 @@ int main(int argc, char const *argv[]) {
         }
 
         // quantum option
-        if( strcmp(argv[i], "-quantum") == 0 && algo == 4 ){
+        if (strcmp(argv[i], "-quantum") == 0 && algo == 4 ) {
             // check next argument for quantum size
             if(DEBUG) { printf("Setting Quantum\n"); }
             quantum = atoi(argv[++i]);
         }
 
         // input option
-        if( strcmp(argv[i], "-input") == 0 ){
+        if (strcmp(argv[i], "-input") == 0) {
             // check next argument for file_name
             if(DEBUG) { printf("ATTEMPTING TO OPEN FILE %s\n", argv[i+1]); }
             file_ptr = fopen(argv[i+1], "r");
             i++;
 
             // check if file was opened
-            if(file_ptr == NULL){
+            if (file_ptr == NULL) {
                 fprintf(stderr, "Error: Cannot open file\n");
                 exit(1);
             }
@@ -71,17 +71,17 @@ int main(int argc, char const *argv[]) {
     }
 
     // check if we got an algorithm and input at least
-    if( !algo ){
+    if (!algo) {
         fprintf(stderr, "Error: Missing algorithm\n");
         printf("Usage: ./scheduler -alg [FCFS,SJF,PR,RR] [-quantum [integer(ms)] -input [filename]\n");
         exit(1);
     }
-    else if( !file_ptr ){
+    else if (!file_ptr) {
         fprintf(stderr, "Error: Missing input file\n");
         printf("Usage: ./scheduler -alg [FCFS,SJF,PR,RR] [-quantum [integer(ms)] -input [filename]\n");
         exit(1);
     }
-    else if( algo == 4 && !quantum ){
+    else if (algo == 4 && !quantum) {
         fprintf(stderr, "Error: Missing quantum time\n");
         printf("Usage: ./scheduler -alg [FCFS,SJF,PR,RR] [-quantum [integer(ms)] -input [filename]\n");
         exit(1);
@@ -93,14 +93,23 @@ int main(int argc, char const *argv[]) {
 
     // create input file parsing thread
     pthread_t file_parser;
-    if( pthread_create(&file_parser, NULL, parse_file, file_ptr) != 0 ){
+    if (pthread_create(&file_parser, NULL, parse_file, file_ptr) != 0) {
         // check if thread was successfully created
-        fprintf(stderr, "Error creating thread\n");
+        fprintf(stderr, "Error creating file_parser thread\n");
         exit(1);
     }
 
-    // join thread and close file
+    // create thread for cpu scheduler
+    pthread_t cpu_thread;
+    if (pthread_create(&cpu_thread, NULL, start_scheduler, &algo) != 0) {
+        // check if thread was successfully created
+        fprintf(stderr, "Error creating CPU_thread");
+        exit(1);
+    };
+
+    // join threads and close file
     pthread_join(file_parser, NULL);
+    pthread_join(cpu_thread, NULL);
     fclose(file_ptr);
 
     // print results
