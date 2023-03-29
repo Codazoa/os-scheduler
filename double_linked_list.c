fb15f77553748ac9bf89eaeb78556c2b9400c317
Drake Farmer <drakeworkes@gmail.com>
2023-03-28 22:17:34 -0700

    added popHighP
    -added popHighP function which returns the proc with the highest
    
    -changed popFirst function to use initialization and allocation to reduce an unnecessary allocation and variable check

--- double_linked_list.c
+++ double_linked_list.c
@@ -30,23 +30,58 @@ void append(DoublyLinkedList *list, Process *new_proc) {
 }
 
 //popFirst
-//This function will remove the head of the doubly linked list
-Process popFirst(DoublyLinkgedList *list){
-
+//This function will return the head process of the doubly linked list
+Process *popFirst(DoublyLinkedList *list){
+    Node *oldHead = list->head;                 //Store the current head  
     
-    if (list->head == NULL){            //Make sure list isn't empty
+    if (oldHead == NULL){                       //Make sure list isn't empty
         return NULL;
     }
-    Node *oldHead = list->head;         //Store the current head         
 
-    if(list->head->next != NULL){       //Is there a next node
-        list->head = list->head->next;  //Make the head's next the new head
-        list->head->prev = NULL;        //Clear the old head from the current head's link
+    if(list->head->next != NULL){               //Is there a next node
+        list->head = list->head->next;          //Make the head's next the new head
+        list->head->prev = NULL;                //Clear the old head from the current head's link
     }
 
-    return oldHead->proc;               //Return the process
+    return oldHead->proc;                       //Return the process
 
 }
 
+//popHighP
+//This function will return the highest priority process
+Process *popHighP(DoublyLinkedList *list){
+    int priorityCounter = 0;                    //Variable to keep track of the highest priority
+    Node *curNode = list->head;                 //Store the head in the curNode variable;                             
+
+    if(curNode == NULL){                     //Make sure the list isn't empty
+        return NULL;
+    }
+
+    //Go through the list and find the highest priority
+    while(curNode != NULL){
+
+        //Overwrite the priorityCounter if the current node has a higher priority
+        if(curNode->proc->priority > priorityCounter){
+            if(curNode->proc->priority == 10){  //Check if this is the highest priority
+                return curNode->proc;           //If it is, just return it
+            }
+            priorityCounter = curNode->proc->priority;
+        }
+        curNode = curNode->next;                //Set curNode to the next node
+    }
+
+    curNode = list->head;                       //Set curNode back to the list's head
+
+    //Now go back through the lsit until we find the highest priority
+    while(curNode != NULL){
+        
+        //Check to see if curNode matches our highest priority
+        if(curNode->proc->priority == priorityCounter){
+            return curNode->proc;               //Return this proc
+        }
+        curNode = curNode->next;                //Set curNode to the next node
+    }
+    return NULL;                                //Backup return in case something messes up
+}
 
 
