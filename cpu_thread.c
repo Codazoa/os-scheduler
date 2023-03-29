ae2c417da61b232051b92f86f25022e401ab0a14
codazoa <codazoa@protonmail.com>
2023-03-28 22:35:50 -0700

    Pseudocode for scheduler

--- cpu_thread.c
+++ cpu_thread.c
@@ -9,14 +9,34 @@
 // if either pipe has a process
     // append it to the ready queue
 
-
+// while ready queue has processes
+    // check algo type
+    // if algo = 1 FCFS
+        // pop first
+    // if algo = 2
+        // pop shortest time
+    // if algo = 3
+        // pop highest priority
+    // if algo = 4
+        // pop first process
+        // if current_time > quantum
+            // sleep for quantum
+            // subtract quantum from time
+            // add back to ready_queue
+        // if current_time <= quantum
+            // sleep for time
+            // increment index
+            // push to io_queue
 
 // start the cpu scheduler
 void *start_scheduler(void *arg) {
     // check the algorithm passed
-    int algo = *(int*)arg;
+    CPU_args_t *cpu_args = (CPU_args_t *)arg;
+    int algo = cpu_args->algo;
+    int quantum = cpu_args->quantum;
 
     printf("Algorithm is %d\n", algo); //DEBUG
+    printf("Quantum is %d\n", quantum);
 
     // create the ready queue
     DoublyLinkedList *ready_queue = create_list();
