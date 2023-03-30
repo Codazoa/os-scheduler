9b54d00ee197a04d10498a4a448d179d800ecb9f
Jack Harvison <jack.harvison@wsu.edu>
2023-03-29 19:38:37 -0700

    DRake ItS BroKEn!

--- cpu_thread.c
+++ cpu_thread.c
@@ -62,16 +62,19 @@ void *start_scheduler(void *arg) {
         switch (algo) {
             case 1: //First Come First Serve
                 // pop first off ready queue
-                // pthread_mutex_lock(&readyq_mtx);
-                // next_proc = popFirst(ready_queue);
-                // pthread_mutex_unlock(&readyq_mtx);
+                pthread_mutex_lock(&readyq_mtx);
+                next_proc = popFirst(ready_queue);
+                pthread_mutex_unlock(&readyq_mtx);
 
                 // printf("New Process\nPriority: %d\n", next_proc->priority);
                 // // sleep for its time
-                // printf("Sleep for %d\n", get_next_burst(next_proc));
-                // sleep(get_next_burst(next_proc));
+                printf("Sleep for %d\n", get_next_burst(next_proc));
+                sleep(get_next_burst(next_proc));
+
                 // push it onto the io queue 
-                // append(ready_queue, next_proc);
+                pthread_mutex_lock(&ioq_mtx);
+                append(io_queue, next_proc);
+                pthread_mutex_unlock(&ioq_mtx);
                 break;
             case 2: //Shortest Job First
                 break;
